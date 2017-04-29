21de1ef3d9ddd2a8a0d4165e7b4505e8f7b4ebb5
CC <ccworld1000@gmail.com>
2017-04-29 18:18:02 +0800

    CCRefreshiOS

---
+++ CCRefreshiOS/CCRefreshiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCRefreshiOS
+//
+//  Created by deng you hua on 4/29/17.
+//  Copyright © 2017 CC | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCRefreshiOS/CCRefreshiOS/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  CCRefreshiOS
+//
+//  Created by deng you hua on 4/29/17.
+//  Copyright © 2017 CC | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
